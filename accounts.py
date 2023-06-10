0ef2f705c12cb89391c3a1c02b34e032e15fb364
pikoloda <pikoloda@student.wszib.edu.pl>
2023-06-10 14:20:32 +0200

    update plik accounts.py

--- accounts.py
+++ accounts.py
@@ -1,7 +1,7 @@
 import pyodbc
 from dotenv  import load_dotenv
 import os
-
+from datetime import datetime
 load_dotenv()
 
 database_password = os.environ.get('DATABASE_PASSWORD')
@@ -22,6 +22,10 @@ connection = pyodbc.connect(connection_string)
 
 class Account:
 
+    @staticmethod
+    def current_time():
+        return datetime.now()
+
     def __init__(self, name: str, open_balance: float = 0.0):
         with connection.cursor() as cursor:
             cursor.execute("INSERT INTO accounts (account_name, account_balance) VALUES (?,?)", (name, open_balance))
@@ -32,30 +36,47 @@ class Account:
             print(f"Account {name}, [id={self.id}] created with opening balance {round(open_balance, 2)}")
 
     def deposit(self, amount: float):
-        if amount > 0:
-
+        if amount < 0:
+            raise ValueError("Wrong amount to deposit")
             with connection.cursor() as cursor:
+
+        cursor = connection.cursor()
+
+        try:
                 self._balance += amount
                 cursor.execute("UPDATE accounts SET account_balance = ? WHERE account_id =?",
                                (self._balance, self.id))
+                cursor.execute(("INSERT INTO transactions (account_id, transaction_time, amount) VALUES (?,?,?)"),
+                               (self.id,Account.current_time(), amount))
                 print(f"{amount} deposited to Account {self.name}")
-
+        except Exception:
+            cursor.rollback()
     def withdraw(self, amount: float):
-        if 0 < amount <= self._balance:
-            with connection.cursor() as cursor:
+        if amount < 0 or amount > self._balance:
+            raise ValueError("Wrong amount to withdraw")
+        cursor = connection.cursor()
+
+        try:
                 self._balance -= amount
                 cursor.execute("UPDATE accounts SET account_balance = ? WHERE account_id =?",
                                (self._balance, self.id))
+                cursor.execute(("INSERT INTO transactions (account_id, transaction_time, amount) VALUES (?,?,?)"),
+                               (self.id, Account.current_time(), -amount))
                 print(f"{amount} withraw from account {self.name}")
-
+        except Exception:
+            cursor.rollback()
+        finally:
+            cursor.cl
     def show_balance(self):
         print(f"Account {self.name} balance: {self._balance}")
 
+def do_transaction(account_from: Account, account_to: Account, amount: float):
+
+
 
 if __name__ == '__main__':
-    account = Account('Piotr')
-    account.deposit(10)
-    account.deposit(0.1)
-    account.deposit(0.3)
-    account.deposit(7.2)
-    account.show_balance()
+    account = Account('Piotr',100)
+    account_two = Account('Maciej', 50)
+    do_transaction(account_two, account, 51)
+
+    connection.close()
